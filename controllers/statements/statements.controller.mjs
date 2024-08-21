9935cc9c6b2caa6a3e457e28fa4a10ba67a9ee1c
AilItAssistant <marketingassistant@ailmadrid.com>
2024-08-21 15:58:35 +0200

    end day

--- controllers/statements/statements.controller.mjs
+++ controllers/statements/statements.controller.mjs
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { pool } from "../../index.mjs"
-import { statements } from "../../routes/stataments/statements.routes.mjs";
+
 
 //?GET ALL STATEMENTS
 
@@ -56,6 +56,64 @@ export const postStatements = async (req, res) => {
         console.log(rows)
         
         res.json(200);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        if (conn) return conn.end();
+    }
+};
+
+//?GET STATEMENTS AND DETAILS
+
+export const getStatementsAndDetails = async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        let statements = await conn.query(
+            "select s.id as statement_id, s.text as statement_text, s.content as statement_content, l.name as level_name, sk.name as skill_name from statements s left join levels l on s.level_id = l.id left join skills sk on s.skill_id = sk.id order by s.id;");
+        let questions = await conn.query(
+            "select q.id as question_id, q.statement_id as statement_id, q.content as question_content, l.name as level_name, sk.name as skill_name, b.name as block_name from questions q left join levels l on q.level_id = l.id left join skills sk on q.skill_id = sk.id left join blocks b on q.block_id = b.id order by q.id; "
+        );
+        let answers = await conn.query("select id, content, is_correct, explanation, question_id from answers; ");
+
+        statements.forEach(element => {
+            if(element.statement_id){element.statement_id = element.statement_id.toString();}
+        });
+
+        questions.forEach(element => {
+            element.question_id = element.question_id.toString(); 
+            if(element.statement_id){element.statement_id = element.statement_id.toString();}
+        });
+
+        answers.forEach(element => {
+            element.id = element.id.toString();
+            if(element.question_id){element.question_id = element.question_id.toString();}
+        });
+
+        let resp = statements;
+
+        for(let i = 0; statements.length > i; i++){
+            for(let x = 0; questions.length > x; x++){
+                if(statements[i].id === questions[x].question_id){
+                    console.log(statements[i].id);
+                    console.log(questions[x].question_id);
+                    resp.statements[i].push(questions[i]);
+                }
+                for(let y = 0; answers.length > y; y++){
+                    if(questions[x].id === answers[y].question_id){
+                        console.log(questions[x].id);
+                        console.log(answers[y].question_id);
+                        resp.statements[i].questions[x].answers[y].push(answers[y]);
+                    }
+                }
+            }
+        }
+        //console.log(statements)
+
+
+        //let resp = []; resp.push(statements); resp.push(questions); resp.push(answers);
+        console.log(resp)
+        res.json(resp);
     } catch (error) {
         console.log(error);
     } finally {
--- routes/stataments/statements.routes.mjs
+++ routes/stataments/statements.routes.mjs
@@ -3,13 +3,17 @@ import { Router } from "express";
 import {
     getStatements,
     getStatementsById,
-    postStatements
+    postStatements,
+    getStatementsAndDetails
 } from "../../controllers/statements/statements.controller.mjs";
 
 export const statements = Router();
 
 //?GET ALL STATEMENTS
 statements.get("/", getStatements);
+
+//?POST STATEMENTS
+statements.get("/details", getStatementsAndDetails);
 
 //?GET STATEMENTS BY ID
 statements.get("/:statementId", getStatementsById);
