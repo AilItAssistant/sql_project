c4a5435db108c68c227c1a7472f7de3c849298a5
AilItAssistant <marketingassistant@ailmadrid.com>
2024-08-28 09:12:19 +0200

    add status to get alumnos

--- controllers/alumnos/alumnos.controller.mjs
+++ controllers/alumnos/alumnos.controller.mjs
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { pool } from "../../index.mjs"
+import { pool } from "../../index.mjs";
 
 //?GET ALL ALUMNOS alumnos.get("/", getAlumnos);
 
@@ -7,50 +7,56 @@ export const getAlumnos = async (req, res) => {
     let conn;
     try {
         conn = await pool.getConnection();
-        let rows = await conn.query("select s.id as student_id, s.name as student_name, s.phone_number as student_phone_number, s.last_name as student_last_name, s.email, s.level as student_level, s.city, s.identification_document, coalesce(c.name, 'No tiene clase') as class_name, coalesce(c.id, 'No tiene id') as class_id, coalesce(c.level, 'N/A') as class_level, coalesce(t.name, 'N/A') as teacher_name from students s left join student_classes sc on s.id = sc.student_id left join classes c on sc.class_id = c.id left join teachers t on c.teacher_id = t.id order by s.id, c.id; ");
-        
-        rows.forEach(element => {
+        let rows = await conn.query(
+            "SELECT s.id AS student_id, s.name AS student_name, s.phone_number AS student_phone_number, s.last_name AS student_last_name, s.email, s.level AS student_level, s.city, s.identification_document, s.status AS student_status, COALESCE(c.name, 'No tiene clase') AS class_name, COALESCE(c.id, 'No tiene id') AS class_id, COALESCE(c.level, 'N/A') AS class_level, COALESCE(t.name, 'N/A') AS teacher_name FROM students s LEFT JOIN student_classes sc ON s.id = sc.student_id LEFT JOIN classes c ON sc.class_id = c.id LEFT JOIN teachers t ON c.teacher_id = t.id ORDER BY s.id, c.id;"
+        );
+        console.log(rows)
+        rows.forEach((element) => {
             element.student_id = element.student_id.toString();
         });
         let response = [];
-        for(let i = 0; rows.length > i; i++) {
+        for (let i = 0; rows.length > i; i++) {
             let x;
-            if(i >= 1) {x = i - 1;} else {x = 0;}
+            if (i >= 1) {
+                x = i - 1;
+            } else {
+                x = 0;
+            }
 
-            if(i === 0 || rows[i].student_id !== rows[x].student_id){
-                
+            if (i === 0 || rows[i].student_id !== rows[x].student_id) {
                 let add = {
                     student_id: rows[i].student_id,
-                    email: rows[i].email, 
-                    phone_number: rows[i].student_phone_number, 
+                    email: rows[i].email,
+                    phone_number: rows[i].student_phone_number,
                     last_name: rows[i].student_last_name,
-                    id_document: rows[i].identification_document, 
+                    id_document: rows[i].identification_document,
                     student_level: rows[i].student_level,
                     name: rows[i].student_name,
                     city: rows[i].city,
                     student_phone: rows[i].student_phone,
-                    classes: []
+                    student_status: rows[i].student_status,
+                    classes: [],
                 };
                 response.push(add);
-                if(rows[i].class_id !== 'No tiene id'){
+                if (rows[i].class_id !== "No tiene id") {
                     let addClass = {
                         class_id: rows[i].class_id,
                         class_level: rows[i].class_level,
                         class_name: rows[i].class_name,
-                        class_teacher: rows[i].teacher_name
+                        class_teacher: rows[i].teacher_name,
                     };
-                    add.classes.push(addClass); 
+                    add.classes.push(addClass);
                 }
             } else {
                 let add = {
                     class_id: rows[i].class_id,
                     class_name: rows[i].class_name,
                     class_level: rows[i].class_level,
-                    class_teacher: rows[i].teacher_name
+                    class_teacher: rows[i].teacher_name,
                 };
-                response[response.length -1].classes.push(add);
-            };
-        };
+                response[response.length - 1].classes.push(add);
+            }
+        }
         res.json(response);
     } catch (error) {
         console.log(error);
