2b854676e15754216406649816e9111ee771ca45
AilItAssistant <marketingassistant@ailmadrid.com>
2024-09-02 15:50:34 +0200

    manage users search

--- controllers/users/users.controller.mjs
+++ controllers/users/users.controller.mjs
@@ -22,3 +22,35 @@ export const getUsers = async (req, res) => {
         if (conn) return conn.end();
     }
 };
+
+export const filterUsers = async (req, res) => {
+    let conn;
+    try {
+        console.log(req.body)
+        conn = await pool.getConnection();
+        let rows = await conn.query(
+            `SELECT 
+                id, username, email, role, created_at, name, last_name, phone_number, city, permissions, status 
+            from 
+                users 
+            WHERE 
+                (last_name LIKE CONCAT(IFNULL('${req.body.last_name}', ''), '%')) OR
+                (username LIKE CONCAT(IFNULL('${req.body.username}', ''), '%')) OR
+                (phone_number LIKE CONCAT(IFNULL('${req.body.phone_number}', ''), '%')) OR
+                (city LIKE CONCAT(IFNULL('${req.body.city}', ''), '%')) OR
+                (permissions LIKE CONCAT(IFNULL('${req.body.permissions}', ''), '%')) OR
+                (status LIKE CONCAT(IFNULL('${req.body.status}', ''), '%')) OR
+                (email LIKE CONCAT(IFNULL('${req.body.email}', ''), '%'));`
+        );
+        console.log(rows)
+        rows.forEach((element) => {
+            element.id = element.id.toString();
+            element.created_at = element.created_at.toLocaleDateString('es-ES', {year: 'numeric', month: 'numeric', day: 'numeric',});
+        });
+        res.json(rows);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        if (conn) return conn.end();
+    }
+};
--- routes/users/users.routes.mjs
+++ routes/users/users.routes.mjs
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 import {
     getUsers,
-    
+    filterUsers
 } from "../../controllers/users/users.controller.mjs";
 
 export const users = Router();
 
 //?GET ALL USERS
-
 users.get("/", getUsers);
+
+//?GET ALL USERS WITH FILTERS
+users.put("/filter", filterUsers);
