f4115bd576f2c5530429e3c345d341d71eab4e96
Jaime Alcalde <jaime.alcalde.n@gmail.com>
2024-10-21 21:17:50 +0200

    end day

--- controllers/answers/answers.controller.mjs
+++ controllers/answers/answers.controller.mjs
@@ -27,3 +27,45 @@ export const getAnswerById = async (req, res) => {
         };
     };
 };
+
+export const editAnswers = async (req, res) => {
+    console.log(req.body)
+    if ( req.data ) {
+        let conn;
+        try {
+            conn = await pool.getConnection();
+            let photo_id = null;
+            if(req.body.photo ) {
+                let photo_id = await conn.query(`
+                    INSERT INTO
+                        photos (
+                            base64_data
+                        )
+                    VALUES (
+                        '${req.body.photo}'
+                    );`);
+                    id = await conn.query(`
+                    SELECT
+                        LAST_INSERT_ID()
+                    AS
+                        last_id;
+                `);
+                photo_id = photo_id[0].last_id.toString()
+            };
+            let rows = await conn.query(`
+                UPDATE
+                    answers
+                SET
+                    content = CASE WHEN ${req.body.content} IS NOT NULL THEN '${req.body.content}' ELSE content END,
+                    photo_id = CASE WHEN ${photo_id} IS NOT NULL THEN '${photo_id}' ELSE photo_id END
+                WHERE
+                    id = ${req.body.id};
+                `);
+            res.json(200);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            if (conn) return conn.end();
+        }
+    }
+};
--- routes/answers/answers.routes.mjs
+++ routes/answers/answers.routes.mjs
@@ -2,10 +2,14 @@ import { Router } from "express";
 
 import { verifyToken } from "../../controllers/users/users.controller.mjs";
 import {
-    getAnswerById
+    getAnswerById,
+    editAnswers
 } from "../../controllers/answers/answers.controller.mjs";
 
 export const answers = Router();
 
 //?GET ANSWERS BY ID
 answers.put("/getById", verifyToken, getAnswerById);
+
+//?EDIT ANSWERS
+answers.put("/edit", verifyToken, editAnswers);
