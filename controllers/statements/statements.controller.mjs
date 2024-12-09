2092d9ab273f48d8415c8112c1c0d5ea7f0d0e14
AilItAssistant <marketingassistant@ailmadrid.com>
2024-12-09 15:31:43 +0100

    end day

--- controllers/statements/statements.controller.mjs
+++ controllers/statements/statements.controller.mjs
@@ -273,7 +273,7 @@ export const editStatements = async (req, res) => {
         let conn;
         let photoId = null;
         if(req.body.content === undefined || req.body.content === "null") {req.body.content = null};
-        if(req.body.text === undefined  || req.body.text === "null") {req.body.link = null};
+        if(req.body.text === undefined  || req.body.text === "null") {req.body.text = null};
         try {
             conn = await pool.getConnection();
             if(req.body.photo ) {
@@ -282,9 +282,8 @@ export const editStatements = async (req, res) => {
                     VALUES ('${req.body.photo}');
                 `);
                 photoId = photo_id.insertId.toString();
-            } else {
-                let photo_id = null;
             };
+            console.log(photoId)
             await conn.query(`
                 UPDATE
                     statements
@@ -292,9 +291,9 @@ export const editStatements = async (req, res) => {
                     level_id = CASE WHEN ${req.body.level_id} IS NOT NULL THEN '${req.body.level_id}' ELSE level_id END,
                     skill_id = CASE WHEN ${req.body.skill_id} IS NOT NULL THEN '${req.body.skill_id}' ELSE skill_id END,
                     score = CASE WHEN ${req.body.score} IS NOT NULL THEN '${req.body.score}' ELSE score END,
-                    text = CASE WHEN '${req.body.text}' IS NOT NULL THEN '${req.body.text}' ELSE text END,
-                    content = CASE WHEN '${req.body.content}' IS NOT NULL AND '${req.body.content}' ELSE content END,
-                    photo_id = CASE WHEN ${photoId} IS NOT NULL THEN '${photoId}' ELSE photo_id END
+                    text = CASE WHEN '${req.body.text}' IS NOT NULL AND '${req.body.text}' != 'null' THEN '${req.body.text}' ELSE text END,
+                    content = CASE WHEN '${req.body.content}' IS NOT NULL AND '${req.body.content}' != 'null' THEN '${req.body.content}' ELSE content END,
+                    photo_id = CASE WHEN ${photoId} IS NOT NULL THEN ${photoId} ELSE photo_id END
                 WHERE
                     id = ${req.body.id};
                 `);
